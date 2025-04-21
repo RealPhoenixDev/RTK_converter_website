b1fe6aec319853a26c815994e743b4632460c5d0
PhoenixDev <147312817+RealPhoenixDev@users.noreply.github.com>
2025-04-21 16:27:36 +0300

    daily task layout

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+const usename_span = document.getElementById("username_span");
+
 function loadUsernameData() {
     const username_window_popup = document.getElementById(
         "username_select_container"
@@ -12,10 +14,36 @@ function loadUsernameData() {
 
     if (!localStorage.getItem("username")) {
         username_window_popup.style.display = "block";
+        username_input.addEventListener("keypress", function (event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                username_submit.click();
+            }
+        });
         username_submit.addEventListener("click", (e) => {
             const username = username_input.value;
             localStorage.setItem("username", username);
             username_window_popup.style.display = "none";
+            username_span.innerHTML = localStorage.getItem("username");
         });
     }
 }
+
+username_span.innerHTML = localStorage.getItem("username");
+
+const data_structure_template = {
+    folder_name: [
+        {
+            task_name: "",
+            description: "",
+            tags: [],
+            date: "date",
+            color: "#FFFFFF",
+            type: "text | checkbox",
+            status: "default | pending | completed",
+        },
+        { task_name: "" },
+        { task_name: "" },
+        { task_name: "" },
+    ],
+};
